cff3156f5f006ec08a6f235a21738820e4b10da9
alanmaranto <alanarostegui@hotmail.com>
2026-06-28 14:34:29 -0600

    test: agregar regresión de colonias duplicadas y migrar a assertEqual

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,7 +11,6 @@ import json
 
 from django.test import TestCase
 from django.urls import reverse
-from mock import patch, MagicMock, Mock
 
 from django_postalcodes_mexico.models import PostalCode
 
@@ -37,7 +36,7 @@ class TestDjango_postalcodes_mexico(TestCase):
     def test_api_returns_postal_code_data(self):
         retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("01000",))
         response = self.client.get(retrieve_postal_code_data)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         postal_code_data = json.loads(response.content)
         self.assertIsInstance(postal_code_data, dict)
         self.assertIn("colonias", postal_code_data)
@@ -48,14 +47,14 @@ class TestDjango_postalcodes_mexico(TestCase):
     def test_api_returns_404_on_not_found_postal_code(self):
         retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("99999",))
         response = self.client.get(retrieve_postal_code_data)
-        self.assertEquals(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
         postal_code_data = json.loads(response.content)
-        self.assertEquals(len(postal_code_data), 0)
+        self.assertEqual(len(postal_code_data), 0)
 
     def test_api_returns_400_on_alpha_input(self):
         retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("ABCDE",))
         response = self.client.get(retrieve_postal_code_data)
-        self.assertEquals(response.status_code, 400)
+        self.assertEqual(response.status_code, 400)
         postal_code_data = json.loads(response.content)
         self.assertNotIn("colonia", postal_code_data)
         self.assertIn("postal_code", postal_code_data)
@@ -63,10 +62,35 @@ class TestDjango_postalcodes_mexico(TestCase):
     def test_api_returns_400_on_long_digit_input(self):
         retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("999999999999",))
         response = self.client.get(retrieve_postal_code_data)
-        self.assertEquals(response.status_code, 400)
+        self.assertEqual(response.status_code, 400)
         postal_code_data = json.loads(response.content)
         self.assertNotIn("colonia", postal_code_data)
         self.assertIn("postal_code", postal_code_data)
+
+    def test_colonias_are_deduplicated_when_sepomex_has_multiple_segments_per_asenta(self):
+        for i in range(5):
+            PostalCode.objects.create(
+                d_codigo='03103',
+                d_asenta='Del Valle Norte',
+                D_mnpio='Benito Juárez',
+                d_ciudad='Ciudad de México',
+                d_CP='03103',
+                c_estado='09',
+                c_oficina='03103',
+                c_tipo_asenta='09',
+                c_mnpio='014',
+                id_asenta_cpcons=f'{i:04d}',
+                d_zona='Urbano',
+                c_cve_ciudad='01',
+            )
+
+        retrieve_postal_code_data = reverse(
+            "django_postalcodes_mexico:get-postal-code-data", args=("03103",)
+        )
+        response = self.client.get(retrieve_postal_code_data)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        self.assertEqual(data['colonias'], ['Del Valle Norte'])
 
     def tearDown(self):
         pass
